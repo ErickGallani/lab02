b210ed70ee3f835fb60d54d998af9c443470801d
Leandro Ramos <le.oliveira.ramos@gmail.com>
2016-10-14 23:09:45 -0300

    Alterações no UC14

--- bookstore-correios/src/test/java/br/unicamp/bookstore/steps/CalcFreteTempoSteps.java
+++ bookstore-correios/src/test/java/br/unicamp/bookstore/steps/CalcFreteTempoSteps.java
@@ -15,62 +15,63 @@ import cucumber.api.java.en.When;
 public class CalcFreteTempoSteps {
 
 	@Given("^que solicito ao sistema para calcular o frete e o tempo de entrega$")
-	public void que_solicito_ao_sistema_para_calcular_o_frete_e_o_tempo_de_entrega() throws Throwable {
+	public void que_solicito_ao_sistema_para_calcular_o_frete_e_o_tempo_de_entrega()  {
 
 	}
 
 	@When("^o sistema envia os dados do produto produto o tipoDeEntrega e CEP$")
-	public void o_sistema_envia_os_dados_do_produto_produto_o_tipoDeEntrega_e_CEP() throws Throwable {
+	public void o_sistema_envia_os_dados_do_produto_produto_o_tipoDeEntrega_e_CEP() {
 
 	}
 
 	@Then("^o sistema recebe o valor_do_frete e tempo_de_entrega$")
-	public void o_sistema_recebe_o_valor_do_frete_e_tempo_de_entrega() throws Throwable {
+	public void o_sistema_recebe_o_valor_do_frete_e_tempo_de_entrega()  {
 
 	}
 
 	@Then("^os valores sao salvos no sistema$")
-	public void os_valores_sao_salvos_no_sistema() throws Throwable {
-
+	public void os_valores_sao_salvos_no_sistema() {
+	 
 	}
 
 	@Given("^um produto valido$")
-	public void um_produto_valido() throws Throwable {
-
+	public void um_produto_valido()  {
+	  
 	}
 
 	@When("^o cliente solicita ao sistema calucar o valor do frete e informa um CEP invalido$")
-	public void o_cliente_solicita_ao_sistema_calucar_o_valor_do_frete_e_informa_um_CEP_invalido() throws Throwable {
+	public void o_cliente_solicita_ao_sistema_calucar_o_valor_do_frete_e_informa_um_CEP_invalido() {
 
 	}
 
 	@Then("^o sistema apresenta um erro com a mensagem$")
-	public void o_sistema_apresenta_um_erro_com_a_mensagem(String arg1) throws Throwable {
-
+	public void o_sistema_apresenta_um_erro_com_a_mensagem()  {
+	 
 	}
 
 	@Given("^um produto e CEP valido$")
-	public void um_produto_e_CEP_valido() throws Throwable {
-
+	public void um_produto_e_CEP_valido() {
+	
 	}
 
 	@When("^o cliente solicita ao sistema calucar o valor do frete e informa um CEP valido$")
-	public void o_cliente_solicita_ao_sistema_calucar_o_valor_do_frete_e_informa_um_CEP_valido() throws Throwable {
+	public void o_cliente_solicita_ao_sistema_calucar_o_valor_do_frete_e_informa_um_CEP_valido() {
 
 	}
 
+	@Then("^o sistema apresenta o erro$")
+	public void o_sistema_apresenta_o_erro()  {
+	
+	}
+
 	@When("^o cliente solicita ao sistema que calcule o tempo do frete$")
-	public void o_cliente_solicita_ao_sistema_que_calcule_o_tempo_do_frete() throws Throwable {
+	public void o_cliente_solicita_ao_sistema_que_calcule_o_tempo_do_frete() {
 
 	}
 
 	@Then("^o sistema retorna com o tempo invalido$")
-	public void o_sistema_retorna_com_o_tempo_invalido(String arg1) throws Throwable {
-
+	public void o_sistema_retorna_com_o_tempo_invalido()  {
+	 
 	}
 
-	@Given("^um produto medidas invalidas e CEP valido$")
-	public void um_produto_medidas_invalidas_e_CEP_valido() throws Throwable {
-
-	}
 }
--- bookstore-correios/src/test/resources/features/CalcFreteTempo.feature
+++ bookstore-correios/src/test/resources/features/CalcFreteTempo.feature
@@ -11,30 +11,18 @@ Scenario: CEP invalido
 	Given um produto valido	
 	When o cliente solicita ao sistema calucar o valor do frete e informa um CEP invalido
 	Then o sistema apresenta um erro com a mensagem
-	"""	
-		O sistema solicita a correcao dos dados.		
-	"""
 
 Scenario: Sistema de serviço do correios retornou erro na validacao do CEP
 	Given um produto e CEP valido
 	When o cliente solicita ao sistema calucar o valor do frete e informa um CEP valido
-	Then o sistema apresenta um erro com a mensagem
-	"""	
-		Se o erro estiver no serviço dos correios:
-			O sistema informa o erro.
-	"""
+	Then o sistema apresenta o erro
 
 Scenario: Erro no tempo de entrega
 	Given um produto e CEP valido
 	When o cliente solicita ao sistema que calcule o tempo do frete
 	Then o sistema retorna com o tempo invalido
-	"""
-		O sistema informa o erro.
-	"""	 
+
 Scenario: Erro no calculo do valor do frete e tempo de entrega
 	Given um produto medidas invalidas e CEP valido
 	When o cliente solicita ao sistema que calcule o tempo do frete
-	Then o sistema apresenta um erro com a mensagem
-	"""
-		O sistema informa o erro.
-	"""	 
+	Then o sistema apresenta o erro 
